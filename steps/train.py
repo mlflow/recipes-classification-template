aeeabd589778985ee7a87b3bcc3a20f1429e87ed
harupy <hkawamura0130@gmail.com>
2022-11-14 12:24:09 +0900

    Fix estimator_params default value
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- steps/train.py
+++ steps/train.py
@@ -7,7 +7,7 @@ This module defines the following routines used by the 'train' step:
 from typing import Dict, Any
 
 
-def estimator_fn(estimator_params: Dict[str, Any] = {}):
+def estimator_fn(estimator_params: Dict[str, Any] = None) -> Any:
     """
     Returns an *unfitted* estimator that defines ``fit()`` and ``predict()`` methods.
     The estimator's input and output signatures should be compatible with scikit-learn
